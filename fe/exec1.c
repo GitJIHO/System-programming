a053fd81764e0bf252a58da15cc089bd8ef2a773
GitJIHO <jiho99322@gmail.com>
2024-06-15 16:24:27 +0900

    add: final exam practice

---
+++ fe/exec1.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+	char *arglist[3];
+	
+	arglist[0] = "ls";
+	arglist[1] = "-l";
+	arglist[2] = 0;
+
+	printf("* * * About to exec ls -l\n");
+	execvp("ls", arglist);
+	printf("* * * ls is done. bye\n");
+
+	return 0;
+}
